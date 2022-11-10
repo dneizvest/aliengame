459f3ad747a059a4c7fda86a595cdae7468c4b5b
dneizvest <116317194+dneizvest@users.noreply.github.com>
2022-11-10 12:18:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// let titleDocument = document.getElementById("title");
+// console.log(titleDocument)
+// let paragraph = document.querySelector(".cool")
+// console.log(paragraph)
+// paragraph.textContent += 'Hello World'
+// console.log(paragraph.textContent)
+// // paragraph.innerHTML += 'comments for <strong>Today</strong>'
+
+// paragraph.innerHTML += `<a href = "google"> Google </a>`
+// console.log(paragraph.innerHTML)
+// titleDocument.style.textAlign = "center";
+// paragraph.style.color = "Blue"
+// console.log(paragraph.getAttribute("class"))
+// console.log(paragraph.getAttribute("id"))
+// console.log(paragraph.setAttribute("id", "spaghetti"))
+// let anchor = document.querySelector("a")
+// anchor.setAttribute("href", "https://www.google.com/")
+// ul#comments>li.comment*3
+
+
+// //store our button into a variable
+// const button = document.querySelector('button');
+// //add an event listener to the button
+// //console log the event
+// button.addEventListener('click', (event)=>{
+//     console.log(event)
+// });
+// document.querySelector('ul').addEventListener('click', handleClick);
+
+
+// function handleClick(event) {
+//     console.log(event);
+// }
+
+console.log("Brad smells like shit")
